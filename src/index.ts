6135ccb520568dbe93a6aea08c817488000c01f4
Carlos Marinho <carlos.marinho20@outlook.com>
2022-03-03 11:54:23 -0300

    Uncomment main module functions

--- src/index.ts
+++ src/index.ts
@@ -5,77 +5,77 @@ import { fromPairs } from "./funcoes/fromPairs";
 import { uniq } from "./funcoes/uniq";
 import { consumirDaFila, escreveNaFila, leArquivo, zerarAquivo } from "./funcoes/fila";
 
-// function testChunk() {
-//   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//   const original = [...items];
+function testChunk() {
+  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const original = [...items];
 
-//   // chamada ao método.
-//   const chunked = chunk(items, 3);
+  // chamada ao método.
+  const chunked = chunk(items, 3);
 
-//   // o array original não pode ser alterado.
-//   const naoAlterados = original.every((item, index) => item === items[index]);
+  // o array original não pode ser alterado.
+  const naoAlterados = original.every((item, index) => item === items[index]);
 
-//   // verificação do método.
-//   console.assert(naoAlterados, 'O array original não pode ser alterado');
-//   console.assert(chunked.length === 4, 'array deve ter 4 grupos');
-//   console.assert(chunked[0].length === 3, 'primeiro grupo deve ter 3 itens');
-//   console.assert(chunked[1].length === 3, 'segundo grupo deve ter 3 itens');
-//   console.assert(chunked[2].length === 3, 'terceiro grupo deve ter 3 itens');
-//   console.assert(chunked[3].length === 1, 'quarto grupo deve ter 1 item');
-// }
+  // verificação do método.
+  console.assert(naoAlterados, "O array original não pode ser alterado");
+  console.assert(chunked.length === 4, "array deve ter 4 grupos");
+  console.assert(chunked[0].length === 3, "primeiro grupo deve ter 3 itens");
+  console.assert(chunked[1].length === 3, "segundo grupo deve ter 3 itens");
+  console.assert(chunked[2].length === 3, "terceiro grupo deve ter 3 itens");
+  console.assert(chunked[3].length === 1, "quarto grupo deve ter 1 item");
+}
 
-// function testCompact() {
-//   const items = [0, 1, false, 2, '', 3, null, undefined, '0'];
-//   const original = [...items];
-//   const expected = [1, 2, 3, '0'];
+function testCompact() {
+  const items = [0, 1, false, 2, "", 3, null, undefined, "0"];
+  const original = [...items];
+  const expected = [1, 2, 3, "0"];
 
-//   // chamada ao método.
-//   const resultado = compact(items);
+  // chamada ao método.
+  const resultado = compact(items);
 
-//   // o array original não pode ser alterado.
-//   const naoAlterados = original.every((item, index) => item === items[index]);
+  // o array original não pode ser alterado.
+  const naoAlterados = original.every((item, index) => item === items[index]);
 
-//   // os itens truthy devem ser mantidos no array resultado.
-//   const resultadoCorreto = expected.every((item, index) => item === resultado[index]);
+  // os itens truthy devem ser mantidos no array resultado.
+  const resultadoCorreto = expected.every((item, index) => item === resultado[index]);
 
-//   // verificação do método.
-//   console.assert(naoAlterados, 'array original não pode ser alterado');
-//   console.assert(resultadoCorreto, 'todos os objetos esperados devem estar presentes');
-// }
+  // verificação do método.
+  console.assert(naoAlterados, "array original não pode ser alterado");
+  console.assert(resultadoCorreto, "todos os objetos esperados devem estar presentes");
+}
 
-// function testFromPairs() {
-//   const items: KeyAndValue[] = [
-//     ['name', 'Edgar Allan Poe'],
-//     ['age', 40],
-//     ['alive', false]
-//   ];
-//   const esperado = {
-//     name: 'Edgar Allan Poe',
-//     age: 40,
-//     alive: false
-//   };
+function testFromPairs() {
+  const items: KeyAndValue[] = [
+    ["name", "Edgar Allan Poe"],
+    ["age", 40],
+    ["alive", false]
+  ];
+  const esperado = {
+    name: "Edgar Allan Poe",
+    age: 40,
+    alive: false
+  };
 
-//   // chamada ao método.
-//   const resultado = fromPairs(items);
+  // chamada ao método.
+  const resultado = fromPairs(items);
 
-//   // verificação do método.
-//   const correto = Object.keys(esperado).every(key => esperado[key] === resultado[key]);
-//   console.assert(correto, 'todos os objetos esperados devem estar presentes');
-// }
+  // verificação do método.
+  const correto = Object.keys(esperado).every((key) => esperado[key] === resultado[key]);
+  console.assert(correto, "todos os objetos esperados devem estar presentes");
+}
 
-// function testUniq() {
-//   const items = [1, 2, 3, 4, 5, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6];
-//   const original = [...items];
-//   const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+function testUniq() {
+  const items = [1, 2, 3, 4, 5, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6];
+  const original = [...items];
+  const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-//   const resultado = uniq(items);
+  const resultado = uniq(items);
 
-//   // verificação do método.
-//   const naoAlterados = original.every((item, index) => item === items[index]);
-//   const resultadoCorreto = expected.every((item, index) => item === resultado[index]);
-//   console.assert(naoAlterados, 'array original não pode ser alterado');
-//   console.assert(resultadoCorreto, 'todos os objetos esperados devem estar presentes');
-// }
+  // verificação do método.
+  const naoAlterados = original.every((item, index) => item === items[index]);
+  const resultadoCorreto = expected.every((item, index) => item === resultado[index]);
+  console.assert(naoAlterados, "array original não pode ser alterado");
+  console.assert(resultadoCorreto, "todos os objetos esperados devem estar presentes");
+}
 
 async function testFila() {
   await zerarAquivo();
@@ -109,8 +109,8 @@ async function testFila() {
   }
 }
 
-// testChunk();
-// testCompact();
-// testFromPairs();
-// testUniq();
+testChunk();
+testCompact();
+testFromPairs();
+testUniq();
 testFila();
